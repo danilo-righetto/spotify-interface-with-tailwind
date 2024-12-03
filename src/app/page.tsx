483a0c18bd3e08246aa73ad024e891417c155603
Danilo Righetto <danilonewtrue@gmail.com>
2024-12-03 16:07:12 -0300

    Botoes da Janela

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,23 @@ export default function Home() {
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
         <aside className="w-72 bg-zinc-950 p-6">
+          <div className="flex items-center gap-2">
+            {/* Botoes da Janela */}
+            <div className="w-3 h-3 bg-red-500 rounded-full" />
+            <div className="w-3 h-3 bg-yellow-500 rounded-full" />
+            <div className="w-3 h-3 bg-green-500 rounded-full" />
+          </div>
           {/* Menu lateral do Spotify */}
-          <nav>
-            <a href="https://github.com/danilo-righetto">
+          <nav className="space-y-5 mt-10">
+            <a href="https://github.com/danilo-righetto" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
             <HomeIcon />
               Home
             </a>
-            <a href="https://github.com/danilo-righetto">
+            <a href="https://github.com/danilo-righetto" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
               <Search />
               Search
             </a>
-            <a href="https://github.com/danilo-righetto">
+            <a href="https://github.com/danilo-righetto" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
               <Library />
               Your Library
             </a>
