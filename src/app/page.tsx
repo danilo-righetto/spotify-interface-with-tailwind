d594ccf68442a8559cb62d59443ad1ced5e8dd40
Danilo Righetto <danilonewtrue@gmail.com>
2024-12-03 16:23:31 -0300

    Divisor com Playlists

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,18 +13,41 @@ export default function Home() {
           </div>
           {/* Menu lateral do Spotify */}
           <nav className="space-y-5 mt-10">
-            <a href="https://github.com/danilo-righetto" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
+            <a href="https://github.com/danilo-righetto" target="_blank" rel="noopener noreferrer" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
             <HomeIcon />
               Home
             </a>
-            <a href="https://github.com/danilo-righetto" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
+            <a href="https://github.com/danilo-righetto" target="_blank" rel="noopener noreferrer" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
               <Search />
               Search
             </a>
-            <a href="https://github.com/danilo-righetto" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
+            <a href="https://github.com/danilo-righetto" target="_blank" rel="noopener noreferrer" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
               <Library />
               Your Library
             </a>
+          </nav>
+          <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3">
+            {/* Divisor com Playlists*/}  
+            <a href="https://github.com/danilo-righetto" target="_blank" rel="noopener noreferrer" className="text-sm text-zinc-400 hover:text-zinc-100">Hits de Natal</a>
+            <a href="https://github.com/danilo-righetto" target="_blank" rel="noopener noreferrer" className="text-sm text-zinc-400 hover:text-zinc-100">Country Mix</a>
+            <a href="https://github.com/danilo-righetto" target="_blank" rel="noopener noreferrer" className="text-sm text-zinc-400 hover:text-zinc-100">Workout</a>
+            <a href="https://github.com/danilo-righetto" target="_blank" rel="noopener noreferrer" className="text-sm text-zinc-400 hover:text-zinc-100">Deep House</a>
+            <a href="https://github.com/danilo-righetto" target="_blank" rel="noopener noreferrer" className="text-sm text-zinc-400 hover:text-zinc-100">Rock Ballads</a>
+            <a href="https://github.com/danilo-righetto" target="_blank" rel="noopener noreferrer" className="text-sm text-zinc-400 hover:text-zinc-100">Top Plays</a>
+            <a href="https://github.com/danilo-righetto" target="_blank" rel="noopener noreferrer" className="text-sm text-zinc-400 hover:text-zinc-100">Top Hits de 2002</a>
+            <a href="https://github.com/danilo-righetto" target="_blank" rel="noopener noreferrer" className="text-sm text-zinc-400 hover:text-zinc-100">Binaural Beats: Focus</a>
+            <a href="https://github.com/danilo-righetto" target="_blank" rel="noopener noreferrer" className="text-sm text-zinc-400 hover:text-zinc-100">Nickelback</a>
+            <a href="https://github.com/danilo-righetto" target="_blank" rel="noopener noreferrer" className="text-sm text-zinc-400 hover:text-zinc-100">Sextou</a>
+            <a href="https://github.com/danilo-righetto" target="_blank" rel="noopener noreferrer" className="text-sm text-zinc-400 hover:text-zinc-100">Electronic Focus</a>
+            <a href="https://github.com/danilo-righetto" target="_blank" rel="noopener noreferrer" className="text-sm text-zinc-400 hover:text-zinc-100">Soft Pop Hits</a>
+            <a href="https://github.com/danilo-righetto" target="_blank" rel="noopener noreferrer" className="text-sm text-zinc-400 hover:text-zinc-100">Gym</a>
+            <a href="https://github.com/danilo-righetto" target="_blank" rel="noopener noreferrer" className="text-sm text-zinc-400 hover:text-zinc-100">zumba 2024</a>
+            <a href="https://github.com/danilo-righetto" target="_blank" rel="noopener noreferrer" className="text-sm text-zinc-400 hover:text-zinc-100">Travel Mix</a>
+            <a href="https://github.com/danilo-righetto" target="_blank" rel="noopener noreferrer" className="text-sm text-zinc-400 hover:text-zinc-100">Carvanal 2025</a>
+            <a href="https://github.com/danilo-righetto" target="_blank" rel="noopener noreferrer" className="text-sm text-zinc-400 hover:text-zinc-100">Hits de Ano Novo</a>
+            <a href="https://github.com/danilo-righetto" target="_blank" rel="noopener noreferrer" className="text-sm text-zinc-400 hover:text-zinc-100">Modão 2024</a>
+            <a href="https://github.com/danilo-righetto" target="_blank" rel="noopener noreferrer" className="text-sm text-zinc-400 hover:text-zinc-100">Casamento</a>
+            <a href="https://github.com/danilo-righetto" target="_blank" rel="noopener noreferrer" className="text-sm text-zinc-400 hover:text-zinc-100">Romantics</a>
           </nav>
         </aside>
         <main className="flex-1 p-6">
