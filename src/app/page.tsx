c4a551fcb9127461188ae38faf1e9fc2562c0299
Danilo Righetto <danilonewtrue@gmail.com>
2024-12-03 12:57:10 -0300

    Criando a estrutura inicial do projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main></main>
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        <aside className="w-72 bg-zinc-950 p-6">
+          {/* Menu lateral do Spotify */}
+          <nav>
+            
+          </nav>
+        </aside>
+        <main className="flex-1 p-6">
+          {/* Area principal do Spotify */}
+          main
+        </main>
+      </div>
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
+        {/* Player do Spotify */}
+        footer
+      </footer>
+    </div>
   );
 }
