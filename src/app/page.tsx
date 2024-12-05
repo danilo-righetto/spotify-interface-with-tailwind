4add21b243df1aa93251eb11c4f0c7d62d306dfe
Danilo Righetto <danilonewtrue@gmail.com>
2024-12-05 17:41:00 -0300

    Player finalizado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play } from 'lucide-react' ;
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat } from 'lucide-react' ;
 import Image from 'next/image';
 
 export default function Home() {
@@ -68,42 +68,42 @@ export default function Home() {
             <a href="https://www.linkedin.com/in/danilo-righetto/" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
               <Image src="/album-1.jpg" width={104} height={104} alt="Capa do álbum All The Right Reasons da banda Nickelback" />
               <strong>All The Right Reasons</strong>
-              <button className="flex items-center justify-center p-2 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
                 <Play fill="black" />
               </button>
             </a>
             <a href="https://www.linkedin.com/in/danilo-righetto/" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
               <Image src="/album-1.jpg" width={104} height={104} alt="Capa do álbum All The Right Reasons da banda Nickelback" />
               <strong>All The Right Reasons</strong>
-              <button className="flex items-center justify-center p-2 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
                 <Play fill="black" />
               </button>
             </a>
             <a href="https://www.linkedin.com/in/danilo-righetto/" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
               <Image src="/album-1.jpg" width={104} height={104} alt="Capa do álbum All The Right Reasons da banda Nickelback" />
               <strong>All The Right Reasons</strong>
-              <button className="flex items-center justify-center p-2 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
                 <Play fill="black" />
               </button>
             </a>
             <a href="https://www.linkedin.com/in/danilo-righetto/" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
               <Image src="/album-1.jpg" width={104} height={104} alt="Capa do álbum All The Right Reasons da banda Nickelback" />
               <strong>All The Right Reasons</strong>
-              <button className="flex items-center justify-center p-2 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
                 <Play fill="black" />
               </button>
             </a>
             <a href="https://www.linkedin.com/in/danilo-righetto/" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
               <Image src="/album-1.jpg" width={104} height={104} alt="Capa do álbum All The Right Reasons da banda Nickelback" />
               <strong>All The Right Reasons</strong>
-              <button className="flex items-center justify-center p-2 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
                 <Play fill="black" />
               </button>
             </a>
             <a href="https://www.linkedin.com/in/danilo-righetto/" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
               <Image src="/album-1.jpg" width={104} height={104} alt="Capa do álbum All The Right Reasons da banda Nickelback" />
               <strong>All The Right Reasons</strong>
-              <button className="flex items-center justify-center p-2 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
                 <Play fill="black" />
               </button>
             </a>
@@ -149,8 +149,24 @@ export default function Home() {
             <span className="text-xs text-zinc-400">Nickelback</span>
           </div>
         </div>
-        <div>
+        <div className="flex flex-col items-center gap-2">
           {/* Player */}
+          <div className="flex items-center gap-3">
+            <Shuffle size={20} className="text-zinc-200" />
+            <SkipBack size={20} className="text-zinc-200" />
+            <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black">
+              <Play fill="black" />
+            </button>
+            <SkipForward size={20} className="text-zinc-200" />
+            <Repeat size={20} className="text-zinc-200" />
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="text-xs text-zinc-400">0:31</span>
+            <div className="h-1 rounded-full w-96 bg-zinc-600">
+              <div className="bg-zinc-200 w-40 h-1 rounded-full"></div>
+            </div>
+            <span className="text-xs text-zinc-400">2:14</span>
+          </div>
         </div>
         <div>
           {/* Recursos do Player */}
